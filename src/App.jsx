03c3792c6a8d7d7023a5dda96907cf6c245e9980
shahiutsav <92775677+shahiutsav@users.noreply.github.com>
2022-03-10 17:47:03 +0545

    General styles layout created for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Header from './components/header/Header.jsx'
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonial from "./components/testimonial/Testimonial";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  return (
-    <div>
-      <Header/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Portfolio />
+            <Testimonial />
+            <Contact />
+            <Footer />
+        </div>
+    );
+};
 
-export default App
+export default App;
