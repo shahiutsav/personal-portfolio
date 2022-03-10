855868ca5494e5af95b89e852f185e9fcad084d4
shahiutsav <92775677+shahiutsav@users.noreply.github.com>
2022-03-10 16:37:02 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/header/Header.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
