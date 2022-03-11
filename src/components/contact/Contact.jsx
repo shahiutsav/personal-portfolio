9b79fa5da8a63d8a46eb32b4dba243e68cb52e4a
shahiutsav <92775677+shahiutsav@users.noreply.github.com>
2022-03-11 19:51:07 +0545

    contace me section completed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,73 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { FaViber } from "react-icons/fa";
 
 const Contact = () => {
-    return <section id="contact">Contact</section>;
+    return (
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option">
+                        <MdOutlineEmail className="contact__option-icon" />
+                        <h4>Email</h4>
+                        <h5>shahiutsav62@gmail.com</h5>
+                        <a href="mailto:shahiutsav62@gmail.com" target="_blank">
+                            Send a Message
+                        </a>
+                    </article>
+                    <article className="contact__option">
+                        <RiMessengerLine className="contact__option-icon" />
+                        <h4>Messenger</h4>
+                        <h5>Utsav Shahi</h5>
+                        <a href="https://m.me/shahi.utsav101" target="_blank">
+                            Send a Message
+                        </a>
+                    </article>
+                    <article className="contact__option">
+                        <FaViber className="contact__option-icon" />
+                        <h4>Viber</h4>
+                        <h5>+977 9861297501</h5>
+                        <a
+                            href="viber://chat?number=+9779861297501"
+                            target="_blank"
+                        >
+                            Send a Message
+                        </a>
+                    </article>
+                </div>
+                {/* End of contact options */}
+
+                <form action="">
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Your Full Name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your Email"
+                        required
+                    />
+                    <textarea
+                        name="message"
+                        rows="7"
+                        placeholder="Your Message"
+                        required
+                    />
+                    <button type="submit" className="btn btn-primary">
+                        Send Message
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
 };
 
 export default Contact;
